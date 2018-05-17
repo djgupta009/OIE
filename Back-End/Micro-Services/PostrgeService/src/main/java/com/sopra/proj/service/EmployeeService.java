4a5932c30ea1afdad52c9449c37bb875aedea58e
ssonalis <39111027+ssonalis@users.noreply.github.com>
2018-05-17 15:04:09 +0530

    Updating EmployeeService

--- Back-End/Micro-Services/PostrgeService/src/main/java/com/sopra/proj/service/EmployeeService.java
+++ Back-End/Micro-Services/PostrgeService/src/main/java/com/sopra/proj/service/EmployeeService.java
@@ -64,7 +64,7 @@ public class EmployeeService implements IEmployeeService {
 	    employeerepository.findById(id).map(empToUpdate -> {
 		empToUpdate.setEmp_Desig(emp.getEmp_Desig());
 		empToUpdate.setEmp_Contact(emp.getEmp_Contact());
-		if(!emp.getEmp_Skills().isEmpty())
+	     if(null != emp.getEmp_Skills() && !emp.getEmp_Skills().isEmpty())
 		{
 			emp.getEmp_Skills().forEach(skill -> {skillrepository.findById(skill.getSkill_id()).map
 				(s -> {
